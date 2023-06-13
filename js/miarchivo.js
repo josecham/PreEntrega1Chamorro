7fea94e1f0386e15e1a860f966a12f90dda9a596
josecham <gabriel_influencia74@hotmail.com>
2023-06-12 22:45:11 -0300

    la etiqueta de lo que guardo

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -40,9 +40,60 @@ for (let i = 1; i <= 2; i++) {
     // Informamos el turno asignado usando el número de repetición (i). cuando llega al 2 se corta el bucle
     alert(" Prioridad  N° "+i+" Nombre: "+ingresarNombre);
     }
-    
+
+let nft = prompt("¿que numero de NFT desea comprar del 1 al 12?");
+while( nft !== "" )
+{
+   switch (nft) {
+       case  "1":
+            alert("NFT Nº 1");
+            break;
+       case  "2":
+            alert("NFT Nº 2");
+            break; 
+       case  "3":
+            alert("NFT Nº 3");
+            break;
+       case  "4":
+            alert("NFT Nº 4");
+            break;
+       case  "5":
+            alert("NFT Nº 5");
+            break;
+       case  "6":
+            alert("NFT Nº 6");
+            break;
+       case  "7":
+            alert("NFT Nº 7");
+            break;
+       case  "8":
+            alert("NFT Nº 8");
+            break;
+       case  "9":
+            alert("NFT Nº 9");
+            break;
+       case  "10":
+            alert("NFT Nº 10");
+            break;
+       case  "11":
+            alert("NFT Nº 11");
+            break;
+       case  "12":
+            alert("NFT Nº 12");
+            break;
+            
+       default:
+           alert("escriba el numero de NFT desea comprar del 1 al 12");
+           break; 
+   }
+   
+   nft = prompt("seleccione aceptar para terminar la compra");
+   
+}
+
+   
 // Funcion quedan() para saber cuantos Nft tengo disponibles
-    let nftdisponibles = 12
+    let nftdisponibles = 11
     function quedan(){ 
     console.log("quedan: "+nftdisponibles)
     alert("NFT DISPONIBLES: "+nftdisponibles);
@@ -60,102 +111,4 @@ for (let i = 1; i <= 2; i++) {
     
     
 
-//SWITCH
-/*
-let numeros = parseInt(prompt("Ingresar Numero"));
-switch(numeros) {   case 1:
-    alert("es un uno")
-    break;
-  case 2:
-    alert("es un dos")
-    break;
-  case 3:
-    alert("es un tres")
-    break;
-  default:
-    alert("es mas de tres")
-    break;
- }
 
- 
-
-
-//alert('Bienvenido a esta página')//
-
-let nombreIngresado   = prompt("Ingresar nombre");
-let apellidoIngresado = prompt("Ingresar apellido");
-
-if((nombreIngresado !="") && (apellidoIngresado !="")){
-    alert("Nombre: "+nombreIngresado +"\nApellido: "+apellidoIngresado); 
-    
-}else{
-    alert("Error: Ingresar nombre y apellido");
-}
-
-let edad = Number(prompt('ingrese edad de usuario: '))
-if (edad < 18){
-alert("Es menor de edad")
-} else if(edad === 18){
-alert ("La edad es 18")
-} else if(edad > 18){
-alert ("La edad es mayor 18")
-}
-
-let nombreIngresado2   = prompt("Ingresar nombre");
-
-if((nombreIngresado2 == "ANA") || (nombreIngresado2 =="ana")){
-    alert("El nombre ingresado es Ana"); 
-}else{
-    alert("El nombre ingresado NO ES Ana"); 
-}
-
-for (let index = 0; index < 10; index++) {
-    alert(index)
-    
-}
-
-//me muestra del o al 2
-for (let index = 0; index < 3; index++) {
-    alert(index);
-}
-
-//Solicitamos un valor al usuario y lo multiplica por uno, por3.por5 
-let ingresarNumero = parseInt(prompt("Ingresar Numero"));
-//En cada repetición, calculamos el número ingresado x el número de repetición (i)
-for (let i = 1; i <= 5; i+=2) {
-let resultado = ingresarNumero * i ;
-alert(ingresarNumero +" multiplicado_por "+ i +" = "+ resultado);
-}
-
-//me muestra del 0 al 6 y cuando es igual a 5 corta el bucle con break o lo continua al siguiente en 6 con continue
-for (let index = 0; index <= 6; index++) {
-    if (index === 5) {
-        continue;
-    }
-       alert(index);
-}
-
-for (let i = 1; i <= 2; i++) {
-    // En cada repetición solicitamos un nombre.
-    let ingresarNombre = prompt("Ingresar nombre");
-    // Informamos el turno asignado usando el número de repetición (i). cuando llega al 2 se corta el bucle
-    alert(" Turno  N° "+i+" Nombre: "+ingresarNombre);
-    }
-
-    let entrada = prompt("Ingresar un nombre");
-// //Repetimos hasta que se ingresa "ESC"
-while( entrada == "" )
-{
-   switch (entrada) {
-       case  "":
-            alert("CAMPO OBLIGATORIO");
-            break;
-       default:
-           alert("BIENVENIDO "+ entrada);
-           break; 
-   }
-   
-   entrada = prompt("Ingresar un nombre");
-   
-}
-*/
